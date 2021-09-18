7b1c9f0fc677e4c9258626e8d3bbb9a90e1f2e33
Deborah Otuagomah <debbie.otua@gmail.com>
2021-09-18 21:03:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function buttonClick() {
     i++;
     document.getElementById("number").innerHTML = i + " " + "like(s)";
 }
+
+//second post click interaction
+var i = 0;
+function secondButtonClick() {
+    console.log("we hit line 4")
+    i++;
+    document.getElementById("number2").innerHTML = i + " " + "like(s)";
+}
+
+//third post click interaction
+var i = 0;
+function thirdButtonClick() {
+    console.log("we hit line 4")
+    i++;
+    document.getElementById("number3").innerHTML = i + " " + "like(s)";
+}
